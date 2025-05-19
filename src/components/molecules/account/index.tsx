a96398c52d163619f3b521f2a8636bf389c426de
baotran1311 <baotran13112003@gmail.com>
2025-05-20 00:17:25 +0700

    feat/Tran: Admin-layout-ui

---
+++ src/components/molecules/account/index.tsx
@@ -0,0 +1,28 @@
+import { Avatar } from "antd";
+import { FC } from "react";
+
+interface AccountProps {
+  subTitle?: string;
+  title?: string;
+  src?: string;
+}
+
+const Account: FC<AccountProps> = ({ subTitle, title, src }) => {
+  return (
+    <div className="flex gap-2">
+      <div className="flex flex-col justify-center items-end">
+        <p className="text-sm">{subTitle}</p>
+        <h3 className="text-lg font-medium">{title}</h3>
+      </div>
+      <div className="flex items-center">
+        <Avatar
+          size={"large"}
+          className="h-20 w-20 border-[3px] border-white border-inset shadow-lg"
+          src={src}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Account;
