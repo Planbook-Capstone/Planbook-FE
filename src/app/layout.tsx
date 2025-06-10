e82ab3b0ea6db04093ae6210072eda3610d0197d
baotran1311 <baotran13112003@gmail.com>
2025-06-10 13:24:57 +0700

    Feat/Tran: Integrated Axios and React Query for API and state management; added user login with token handling.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Questrial,Manrope } from "next/font/google";
+import { Geist, Geist_Mono, Questrial, Manrope } from "next/font/google";
 import "./globals.css";
 import { AppProvider } from "@/store";
 import { Suspense } from "react";
 import { Toaster } from "@/components/ui/sonner";
+import ReactQueryProvider from "@/components/providers/ReactQueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,10 +56,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${questrial.variable} ${manrope.variable} antialiased`}
       >
-        <Suspense fallback={<div>Loading...</div>}>
-          <Toaster position="top-right" />
-          <AppProvider>{children}</AppProvider>
-        </Suspense>
+        <ReactQueryProvider>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Toaster position="top-right" />
+            <AppProvider>{children}</AppProvider>
+          </Suspense>
+        </ReactQueryProvider>
       </body>
     </html>
   );
