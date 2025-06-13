86bd3c8e7e735c2df8af675e3e6cadf0d30d0250
baotran1311 <baotran13112003@gmail.com>
2025-06-13 17:07:28 +0700

    Feat/Tran: add update grade and subject modals with form handling

--- src/components/organisms/grade-list/index.tsx
+++ src/components/organisms/grade-list/index.tsx
@@ -1,8 +1,10 @@
 import { DataTable } from "../data-table";
-import { gradeColumns } from "./columns";
+
 import { Row } from "@tanstack/react-table";
 import { GradeResponse } from "@/types";
 import { useGradesService } from "@/services/gradeServices";
+import { useState } from "react";
+import { gradeColumns } from "./columns";
 
 interface GradeTableProps {
   onSelectionChange?: (selectedRows: Row<GradeResponse>[]) => void;
@@ -12,10 +14,12 @@ export default function GradeTable({ onSelectionChange }: GradeTableProps) {
   const { data: grades } = useGradesService();
 
   return (
-    <DataTable
-      columns={gradeColumns}
-      data={grades?.data?.content || []}
-      onSelectionChange={onSelectionChange}
-    />
+    <>
+      <DataTable
+        columns={gradeColumns}
+        data={grades?.data?.content || []}
+        onSelectionChange={onSelectionChange}
+      />
+    </>
   );
 }
