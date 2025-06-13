fa41075151dae79c388a5fc5cb0749a5a8440c80
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2025-06-13 13:23:32 +0700

    Fix/Nhi: Re-migrate Page

---
+++ src/app/grading-test/page.tsx
@@ -0,0 +1,11 @@
+import AutoGradingPage from "@/components/templates/auto-grading";
+import ChatScreenTemplate from "@/components/templates/chat-screen";
+import TestLayout from "@/components/templates/test-layout";
+
+export default function Page() {
+  return (
+    <div className="h-screen">
+      <AutoGradingPage />
+    </div>
+  );
+}
