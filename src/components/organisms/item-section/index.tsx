5bef8bf22630521b29ce1e6133dd565b2f661573
baotran1311 <baotran13112003@gmail.com>
2025-06-14 00:45:24 +0700

    Feat/Tran:Refactor layout and header components for improved responsiveness; update item section styles and sorting options

--- src/components/organisms/item-section/index.tsx
+++ src/components/organisms/item-section/index.tsx
@@ -13,22 +13,20 @@ interface ItemSectionProps {
   action?: React.ReactNode;
 }
 
-export default function ItemSection({ title,action }: ItemSectionProps) {
+export default function ItemSection({ title, action }: ItemSectionProps) {
   return (
-    <div className="flex justify-between items-center">
-      <h1 className="flex items-center justify-start text-xl font-calsans">
-        {title}
-      </h1>
+    <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4">
+      <h1 className="flex gap-1 items-center text-xl font-calsans">{title}</h1>
 
-      <div className="flex justify-end items-center gap-2.5 w-1/2">
+      <div className="flex justify-end items-center gap-2.5 w-full sm:w-1/2">
         <Select defaultValue="light">
-          <SelectTrigger className="w-fit">
-            <SelectValue placeholder="Theme" />
+          <SelectTrigger className="min-w-[120px] w-fit">
+            <SelectValue placeholder="Sắp xếp" />
           </SelectTrigger>
           <SelectContent>
             <SelectItem value="light">Mới nhất</SelectItem>
-            <SelectItem value="dark">Dark</SelectItem>
-            <SelectItem value="system">System</SelectItem>
+            <SelectItem value="dark">Cũ nhất</SelectItem>
+            <SelectItem value="system">Tùy chỉnh</SelectItem>
           </SelectContent>
         </Select>
 
