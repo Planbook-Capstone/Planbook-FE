fd5c9f7a90567439e783487743c2be3de8856286
baotran1311 <baotran13112003@gmail.com>
2025-06-16 14:16:25 +0700

    Feat/Tran: Implement Book Type management with CRUD operations and status handling

--- src/services/bookTypeServices.ts
+++ src/services/bookTypeServices.ts
@@ -1,4 +1,9 @@
-import { createMutationHook, createQueryHook } from "@/hooks/react-query";
+import {
+  createMutationHook,
+  createQueryHook,
+  updateMutationHook,
+  patchMutationHook
+} from "@/hooks/react-query";
 
 export const useBookTypesService = createQueryHook("bookTypes", "/book-type");
 
@@ -6,9 +11,10 @@ export const useCreateBookTypeService = createMutationHook(
   "bookTypes",
   "/book-type"
 );
-// export const useUpdateBookStatus = patchMutationHook("books", "/book");
-// export const useBookTypesService = createQueryHook("bookTypes", "/book");
-// export const useBookByIdService = createQueryWithPathParamHook(
-//   "bookById",
-//   "/book"
-// );
+
+export const useUpdateBookTypeService = updateMutationHook(
+  "bookTypes",
+  "/book-type"
+);
+
+export const useUpdateBookTypeStatus = patchMutationHook("bookTypes", "/book-type");
