18c5afb52c40c85541a07447768881c5e7f83cc5
baotran1311 <baotran13112003@gmail.com>
2025-06-19 20:44:42 +0700

    Feat/Tran: Add LandingPage component with layout and sections

---
+++ src/app/about/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { FeatureCardsSection } from "@/components/organisms/feature-card-section";
+import { Footer } from "@/components/organisms/footer";
+import { LandingPageHeader } from "@/components/organisms/header/LandingPageHeader";
+import { HeroSection } from "@/components/organisms/hero-section";
+import { PartnerSection } from "@/components/organisms/partner-section";
+import { PricingSection } from "@/components/organisms/pricing-section";
+import { ParallaxProvider } from "react-scroll-parallax";
+
+export default function LandingPage() {
+  console.log("test")
+  return (
+    <div className="min-h-screen w-full flex flex-col justify-center">
+      <ParallaxProvider>
+        <LandingPageHeader />
+        <HeroSection />
+        <FeatureCardsSection />
+        <PricingSection />
+        <PartnerSection />
+        <Footer />
+      </ParallaxProvider>
+    </div>
+  );
+}
