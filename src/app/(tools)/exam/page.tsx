ff01f9caaa4f4435a04d3192b2cd3fc954239e99
baotran1311 <baotran13112003@gmail.com>
2025-06-29 13:46:17 +0700

    Feat/Tran: Add Validate Exam Matrix

---
+++ src/app/(tools)/exam/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import MatrixTemplate2 from "@/components/templates/matrix-v2";
+
+function ExamPage() {
+  return (
+    <div className="h-screen space-y-6">
+      <MatrixTemplate2 />
+    </div>
+  );
+}
+
+export default ExamPage;
