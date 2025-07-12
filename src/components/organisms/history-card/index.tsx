03478760c6e5a9a136cedefe2749875f019e25cc
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2025-07-12 22:46:53 +0700

    Feat/Nhi: Icon Picker

--- src/components/organisms/history-card/index.tsx
+++ src/components/organisms/history-card/index.tsx
@@ -16,35 +16,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MoreHorizontal } from "lucide-react";
+import { IconPicker } from "@/components/ui/IconPicker";
+import * as Icons from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
+import * as FaIcons from "react-icons/fa6";
 
 interface HistoryCardProps {
   className?: string;
 }
 
 export default function HistoryCard({ className }: HistoryCardProps) {
-  const icons: IconType[] = [
-    FaBoxArchive,
-    FaBriefcase,
-    FaCalendar,
-    FaCertificate,
-    FaFile,
-    FaFlask,
-  ];
-  const getRandomIcon = (): IconType => {
-    const randomIndex = Math.floor(Math.random() * icons.length);
-    return icons[randomIndex];
-  };
-  const RandomIcon = getRandomIcon();
+  const [dropdownIcon, setDropdownIcon] = useState<string>("FaBookBookmark");
+  const [dropdownIconColor, setDropdownIconColor] = useState("#9ca3af");
+
+  const IconComponent = FaIcons[dropdownIcon as keyof typeof FaIcons];
   return (
     <div className="relative border rounded-md p-4 bg-white shadow-none cursor-pointer hover:shadow-md transition duration-200">
-      <RandomIcon
-        className={cn(
-          "absolute -top-3 h-6 w-6 text-neutral-800 text-shadow-md",
-          className
-        )}
-      />
+      {IconComponent && (
+        <div
+          className={cn(
+            "absolute -top-3 h-6 w-6 text-neutral-800 text-shadow-md",
+            className
+          )}
+        >
+          <IconComponent
+            size={14}
+            color={dropdownIconColor}
+            className="h-6 w-6"
+          />
+        </div>
+      )}
 
       <div className="flex items-center justify-between">
         <h3 className="mt-2 font-calsans text-sm text-black line-clamp-1">
@@ -52,11 +54,36 @@ export default function HistoryCard({ className }: HistoryCardProps) {
         </h3>
         <DropdownMenu>
           <DropdownMenuTrigger className="rounded-full hover:bg-accent p-2 cursor-pointer">
-            <MoreHorizontal className="text-gray-400" size={16} />
+            <Icons.MoreHorizontal className="text-gray-400" size={16} />
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" sideOffset={-10}>
+          <DropdownMenuContent align="end" sideOffset={-10} className="w-80">
+            <DropdownMenuLabel>
+              <div className="flex justify-between items-center">
+                <h6 className="font-calsans text-sm">Tùy chỉnh menu</h6>
+                <div className="gap-3 flex justify-between items-center">
+                  <Button className="p-0 bg-transparent shadow-none text-neutral-800 hover:shadow-none">
+                    Chỉnh sửa
+                  </Button>
+                  <Button className="p-0 bg-transparent shadow-none text-neutral-800 hover:shadow-none">
+                    xoá
+                  </Button>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <div className="p-3">
+              <IconPicker
+                selectedIcon={dropdownIcon as any}
+                selectedColor={dropdownIconColor}
+                onIconChange={setDropdownIcon}
+                onColorChange={setDropdownIconColor}
+              />
+            </div>
+
+            {/* <DropdownMenuSeparator />
             <DropdownMenuItem>Chỉnh sửa tiêu đề</DropdownMenuItem>
-            <DropdownMenuItem>Xóa</DropdownMenuItem>
+            <DropdownMenuItem>Xóa</DropdownMenuItem> */}
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
