d414fd73fe7e01351df5d551b6701c0d7c7a6b30
baotran1311 <baotran13112003@gmail.com>
2025-07-17 13:17:37 +0700

    Style: enhance Sidebar component with full height and improved header padding

--- src/components/demo/Sidebar.tsx
+++ src/components/demo/Sidebar.tsx
@@ -60,10 +60,10 @@ export default function Sidebar({
   const { data: materials } = useMaterialSearchService("1");
 
   return (
-    <div className="w-80 bg-white border-r border-gray-200 flex flex-col ">
+    <div className="w-80 bg-white border-r border-gray-200 flex flex-col h-screen ">
       {/* Sidebar Header */}
       <div className="p-4 border-b border-gray-200">
-        <h2 className="text-lg font-calsans ">Thành phần</h2>
+        <h2 className="text-lg font-calsans py-1.5 ">Thành phần</h2>
       </div>
 
       {/* Tabs */}
