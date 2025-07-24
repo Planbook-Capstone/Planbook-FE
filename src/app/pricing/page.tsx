b77ccd9e922a6d4f9ca98f8c78d7460c78d777bc
baotran1311 <baotran13112003@gmail.com>
2025-07-24 10:34:33 +0700

    Feat/Tran:Add Features Pricing Page

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import MainLayout from "@/components/layout/MainLayout";
 import PricingCard from "@/components/organisms/pricing-card";
+import { Skeleton } from "@/components/ui/skeleton";
+
 import { useSubscriptionsService } from "@/services/subscriptionServices";
 
 function PricingPage() {
-  const { data: subscriptions } = useSubscriptionsService();
+  const { data: subscriptions, isLoading } = useSubscriptionsService();
 
   return (
     <div className="flex-1 justify-center items-center h-screen p-10">
@@ -13,19 +15,27 @@ function PricingPage() {
         <p className="text-xl text-slate-900">
           Tùy chỉnh theo nhu cầu – linh hoạt cho mọi quy mô
         </p>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-5">
-          {/* <PricingCard /> */}
-          {subscriptions?.data?.map((subscription: any) => (
-            <PricingCard
-              key={subscription.id}
-              title={subscription.name}
-              description={subscription.description}
-              price={subscription.price}
-              highlight={subscription.highlight}
-              tokenAmount={subscription.tokenAmount}
-              id={subscription.id}
-            />
-          ))}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-5 w-full">
+          {isLoading ? (
+            <>
+              <Skeleton className="h-[550px] w-full rounded-xl bg-neutral-300" />
+              <Skeleton className="h-[550px] w-full rounded-xl bg-neutral-300" />
+              <Skeleton className="h-[550px] w-full rounded-xl bg-neutral-300" />
+            </>
+          ) : (
+            subscriptions?.data?.map((subscription: any) => (
+              <PricingCard
+                key={subscription.id}
+                title={subscription.name}
+                description={subscription.description}
+                price={subscription.price}
+                highlight={subscription.highlight}
+                tokenAmount={subscription.tokenAmount}
+                id={subscription.id}
+                features={subscription.features}
+              />
+            ))
+          )}
         </div>
       </div>
     </div>
