21fa4dfd3698fd087915c0d0e7a46392603ca18e
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2025-08-07 19:24:01 +0700

    Fix/Nhi: Update Function for BookType

--- src/app/admin/booktype/page.tsx
+++ src/app/admin/booktype/page.tsx
@@ -97,7 +97,8 @@ function BookTypePage() {
             tokenCostPerQuery: selected[0].original.tokenCostPerQuery || 0,
             priority: selected[0].original.priority || 0,
             icon: selected[0].original.icon || undefined,
-            href:selected[0].original?.href || "",
+            href: selected[0].original?.href || "",
+            code: selected[0].original?.code || "",
           }}
         />
       )}
